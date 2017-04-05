1878aa484293ff40e593b065191fd5d0b009867b
Anwar Chirakkattil <achirakkattil@pivotal.io>
2017-04-05 12:36:16 -0500

    Add quota tests

--- export/exportconfig_test.go
+++ export/exportconfig_test.go
@@ -110,5 +110,54 @@ var _ = Describe("Export manager", func() {
 			Ω(len(spaceDetails.Manager.Users)).Should(BeEquivalentTo(0))
 			Ω(len(spaceDetails.Auditor.Users)).Should(BeEquivalentTo(0))
 		})
+
+		It("Exports Quota definition", func() {
+
+			orgId := "org1-1234"
+			spaceId := "dev-1234"
+			userIDToUserMap := make(map[string]uaac.User, 0)
+			orgs := make([]*cc.Org, 0)
+			user1 := uaac.User{ID: "1", Origin: "ldap", UserName: "user1"}
+			userIDToUserMap["user1"] = user1
+			orgQuotaGUID := "54gdgf45454"
+			spaceQuotaGUID := "75gdgf45454"
+			org1 := &cc.Org{Entity: cc.OrgEntity{Name: "org1", QuotaDefinitionGUID: orgQuotaGUID}, MetaData: cc.OrgMetaData{GUID: orgId}}
+			space := &cc.Space{Entity: cc.SpaceEntity{Name: "dev", QuotaDefinitionGUID: spaceQuotaGUID, AllowSSH: true}, MetaData: cc.SpaceMetaData{GUID: spaceId}}
+			orgs = append(orgs, org1)
+			spaces := make([]*cc.Space, 0)
+			spaces = append(spaces, space)
+
+			mockUaac.EXPECT().UsersByID().Return(userIDToUserMap, nil)
+			mockController.EXPECT().ListOrgs().Return(orgs, nil)
+			mockController.EXPECT().ListSpaces(orgId).Return(spaces, nil)
+			cloudControllerOrgUserMock(mockController, orgId, map[string]string{"user1": "1", "user2": "2"}, map[string]string{}, map[string]string{})
+			cloudControllerSpaceUserMock(mockController, spaceId, map[string]string{}, map[string]string{"user1": "1", "user2": "2"}, map[string]string{})
+
+			orgQuota := &cc.Quota{Entity: cc.QuotaEntity{Name: "dummy-org-quota", MemoryLimit: 2, InstanceMemoryLimit: 5}, MetaData: cc.QuotaMetaData{GUID: orgQuotaGUID}}
+			spaceQuota := &cc.Quota{Entity: cc.QuotaEntity{Name: "dummy-space-quota", MemoryLimit: 1, InstanceMemoryLimit: 6}, MetaData: cc.QuotaMetaData{GUID: spaceQuotaGUID}}
+
+			mockController.EXPECT().QuotaDef(orgQuotaGUID, "organizations").Return(orgQuota, nil)
+			mockController.EXPECT().QuotaDef(spaceQuotaGUID, "spaces").Return(spaceQuota, nil)
+
+			err := exportManager.ExportConfig(excludedOrgs, excludedSpaces)
+
+			Ω(err).Should(BeNil())
+			orgDetails := &organization.InputUpdateOrgs{}
+			err = utils.NewDefaultManager().LoadFile("test/config/org1/orgConfig.yml", orgDetails)
+			Ω(err).Should(BeNil())
+			Ω(orgDetails.Org).Should(Equal("org1"))
+			Ω(orgDetails.MemoryLimit).Should(Equal(2))
+			Ω(orgDetails.InstanceMemoryLimit).Should(Equal(5))
+
+			spaceDetails := &sp.InputUpdateSpaces{}
+			err = utils.NewDefaultManager().LoadFile("test/config/org1/dev/spaceConfig.yml", spaceDetails)
+			Ω(err).Should(BeNil())
+			Ω(spaceDetails.Org).Should(Equal("org1"))
+			Ω(spaceDetails.Space).Should(Equal("dev"))
+			Ω(spaceDetails.MemoryLimit).Should(Equal(1))
+			Ω(spaceDetails.InstanceMemoryLimit).Should(Equal(6))
+			Ω(spaceDetails.AllowSSH).Should(BeTrue())
+
+		})
 	})
 })
