7f7f42ede0fcb324ee0db480e7ff5ef30559e172
Zac Bergquist <zbergquist99@gmail.com>
2017-05-20 11:13:35 -0600

    config cleanup
    - remove naked returns
    - annotate errors with extra context
    - employ early returns for failure cases
    
    Note: left a lot of TODOs in here where we should be using filepath.Join instead of Sprintf for creating paths.  This file has poor test coverage so wasn't comfortable making the change yet.

--- config/config.go
+++ config/config.go
@@ -66,120 +66,134 @@ func NewManager(configDir string) Manager {
 }
 
 //AddOrgToConfig -
-func (m *DefaultManager) AddOrgToConfig(orgConfig *OrgConfig) (err error) {
+func (m *DefaultManager) AddOrgToConfig(orgConfig *OrgConfig) error {
 	orgList := &organization.InputOrgs{}
-	orgFileName := fmt.Sprintf("%s/orgs.yml", m.ConfigDir)
+	orgFileName := fmt.Sprintf("%s/orgs.yml", m.ConfigDir) // TODO filepath.Join
 	orgName := orgConfig.OrgName
 	if orgName == "" {
-		err = errors.New("Cannot have an empty org name")
-		return
+		return errors.New("cannot have an empty org name")
 	}
 
-	orgQuota := orgConfig.OrgQuota
-	if err = utils.NewDefaultManager().LoadFile(orgFileName, orgList); err == nil {
-		if orgList.Contains(orgName) {
-			lo.G.Infof("%s already added to config", orgName)
-		} else {
-			lo.G.Infof("Adding org: %s ", orgName)
-			orgList.Orgs = append(orgList.Orgs, orgName)
-			if err = utils.NewDefaultManager().WriteFile(orgFileName, orgList); err == nil {
-				if err = os.MkdirAll(fmt.Sprintf("%s/%s", m.ConfigDir, orgName), 0755); err == nil {
-					orgConfigYml := &organization.InputUpdateOrgs{
-						Org:                     orgName,
-						BillingManager:          organization.UserMgmt{LdapGroup: orgConfig.OrgBillingMgrLDAPGrp, Users: orgConfig.OrgBillingMgrUAAUsers, LdapUsers: orgConfig.OrgBillingMgrLDAPUsers},
-						Manager:                 organization.UserMgmt{LdapGroup: orgConfig.OrgMgrLDAPGrp, Users: orgConfig.OrgMgrUAAUsers, LdapUsers: orgConfig.OrgMgrLDAPUsers},
-						Auditor:                 organization.UserMgmt{LdapGroup: orgConfig.OrgAuditorLDAPGrp, Users: orgConfig.OrgAuditorUAAUsers, LdapUsers: orgConfig.OrgAuditorLDAPUsers},
-						EnableOrgQuota:          orgQuota.IsQuotaEnabled(),
-						MemoryLimit:             orgQuota.GetMemoryLimit(),
-						InstanceMemoryLimit:     orgQuota.GetInstanceMemoryLimit(),
-						TotalRoutes:             orgQuota.GetTotalRoutes(),
-						TotalServices:           orgQuota.GetTotalServices(),
-						PaidServicePlansAllowed: orgQuota.IsPaidServicesAllowed(),
-						RemoveUsers:             true,
-					}
-					utils.NewDefaultManager().WriteFile(fmt.Sprintf("%s/%s/orgConfig.yml", m.ConfigDir, orgName), orgConfigYml)
-					spaces := &space.InputCreateSpaces{
-						Org: orgName,
-					}
-					utils.NewDefaultManager().WriteFile(fmt.Sprintf("%s/%s/spaces.yml", m.ConfigDir, orgName), spaces)
-				}
-			}
-		}
+	mgr := utils.NewDefaultManager()
+	err := mgr.LoadFile(orgFileName, orgList)
+	if err != nil {
+		return err
 	}
-	return
+
+	if orgList.Contains(orgName) {
+		lo.G.Infof("%s already added to config", orgName)
+		return nil
+	}
+	lo.G.Infof("Adding org: %s ", orgName)
+	orgList.Orgs = append(orgList.Orgs, orgName)
+	if err = mgr.WriteFile(orgFileName, orgList); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(fmt.Sprintf("%s/%s", m.ConfigDir, orgName), 0755); err != nil {
+		return err
+	}
+	orgConfigYml := &organization.InputUpdateOrgs{
+		Org:                     orgName,
+		BillingManager:          newUserMgmt(orgConfig.OrgBillingMgrLDAPGrp, orgConfig.OrgBillingMgrUAAUsers, orgConfig.OrgBillingMgrLDAPUsers),
+		Manager:                 newUserMgmt(orgConfig.OrgMgrLDAPGrp, orgConfig.OrgMgrUAAUsers, orgConfig.OrgMgrLDAPUsers),
+		Auditor:                 newUserMgmt(orgConfig.OrgAuditorLDAPGrp, orgConfig.OrgAuditorUAAUsers, orgConfig.OrgAuditorLDAPUsers),
+		EnableOrgQuota:          orgConfig.OrgQuota.IsQuotaEnabled(),
+		MemoryLimit:             orgConfig.OrgQuota.GetMemoryLimit(),
+		InstanceMemoryLimit:     orgConfig.OrgQuota.GetInstanceMemoryLimit(),
+		TotalRoutes:             orgConfig.OrgQuota.GetTotalRoutes(),
+		TotalServices:           orgConfig.OrgQuota.GetTotalServices(),
+		PaidServicePlansAllowed: orgConfig.OrgQuota.IsPaidServicesAllowed(),
+		RemoveUsers:             true,
+	}
+	mgr.WriteFile(fmt.Sprintf("%s/%s/orgConfig.yml", m.ConfigDir, orgName), orgConfigYml) // TODO: filepath.Join
+	spaces := &space.InputCreateSpaces{
+		Org: orgName,
+	}
+	mgr.WriteFile(fmt.Sprintf("%s/%s/spaces.yml", m.ConfigDir, orgName), spaces) // TODO: filepath.Join
+	return nil
+}
+
+func newUserMgmt(ldapGroup string, users, ldapUsers []string) organization.UserMgmt {
+	return organization.UserMgmt{
+		LdapGroup: ldapGroup,
+		Users:     users,
+		LdapUsers: ldapUsers,
+	}
 }
 
 //AddSpaceToConfig -
-func (m *DefaultManager) AddSpaceToConfig(spaceConfig *SpaceConfig) (err error) {
+func (m *DefaultManager) AddSpaceToConfig(spaceConfig *SpaceConfig) error {
+	orgName := spaceConfig.OrgName
+	spaceFileName := fmt.Sprintf("%s/%s/spaces.yml", m.ConfigDir, orgName) // TODO: filepath.Join
 	spaceList := &space.InputCreateSpaces{}
 	spaceName := spaceConfig.SpaceName
-	orgName := spaceConfig.OrgName
-	spaceFileName := fmt.Sprintf("%s/%s/spaces.yml", m.ConfigDir, orgName)
-	spaceQuota := spaceConfig.SpaceQuota
-	if err = utils.NewDefaultManager().LoadFile(spaceFileName, spaceList); err == nil {
-		if spaceList.Contains(spaceName) {
-			lo.G.Infof("%s already added to config", spaceName)
-		} else {
-			lo.G.Infof("Adding space: %s ", spaceName)
-			spaceList.Spaces = append(spaceList.Spaces, spaceName)
-			if err = utils.NewDefaultManager().WriteFile(spaceFileName, spaceList); err == nil {
-				if err = os.MkdirAll(fmt.Sprintf("%s/%s/%s", m.ConfigDir, orgName, spaceName), 0755); err == nil {
-					spaceConfigYml := &space.InputUpdateSpaces{
-						Org:                     orgName,
-						Space:                   spaceName,
-						Developer:               space.UserMgmt{LdapGroup: spaceConfig.SpaceDevLDAPGrp, Users: spaceConfig.SpaceDevUAAUsers, LdapUsers: spaceConfig.SpaceDevLDAPUsers},
-						Manager:                 space.UserMgmt{LdapGroup: spaceConfig.SpaceMgrLDAPGrp, Users: spaceConfig.SpaceMgrUAAUsers, LdapUsers: spaceConfig.SpaceMgrLDAPUsers},
-						Auditor:                 space.UserMgmt{LdapGroup: spaceConfig.SpaceAuditorLDAPGrp, Users: spaceConfig.SpaceAuditorUAAUsers, LdapUsers: spaceConfig.SpaceAuditorLDAPUsers},
-						EnableSpaceQuota:        spaceQuota.IsQuotaEnabled(),
-						MemoryLimit:             spaceQuota.GetMemoryLimit(),
-						InstanceMemoryLimit:     spaceQuota.GetInstanceMemoryLimit(),
-						TotalRoutes:             spaceQuota.GetTotalRoutes(),
-						TotalServices:           spaceQuota.GetTotalServices(),
-						PaidServicePlansAllowed: spaceQuota.IsPaidServicesAllowed(),
-						RemoveUsers:             true,
-						AllowSSH:                spaceConfig.AllowSSH,
-					}
-					utils.NewDefaultManager().WriteFile(fmt.Sprintf("%s/%s/%s/spaceConfig.yml", m.ConfigDir, orgName, spaceName), spaceConfigYml)
-					utils.NewDefaultManager().WriteFileBytes(fmt.Sprintf("%s/%s/%s/security-group.json", m.ConfigDir, orgName, spaceName), []byte("[]"))
-				}
-			}
-		}
+	mgr := utils.NewDefaultManager()
+
+	if err := mgr.LoadFile(spaceFileName, spaceList); err != nil {
+		return err
 	}
-	return
+	if spaceList.Contains(spaceName) {
+		lo.G.Infof("%s already added to config", spaceName)
+		return nil
+	}
+	lo.G.Infof("Adding space: %s ", spaceName)
+	spaceList.Spaces = append(spaceList.Spaces, spaceName)
+	if err := mgr.WriteFile(spaceFileName, spaceList); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(fmt.Sprintf("%s/%s/%s", m.ConfigDir, orgName, spaceName), 0755); err != nil {
+		return err
+	}
+	spaceConfigYml := &space.InputUpdateSpaces{
+		Org:                     orgName,
+		Space:                   spaceName,
+		Developer:               space.UserMgmt{LdapGroup: spaceConfig.SpaceDevLDAPGrp, Users: spaceConfig.SpaceDevUAAUsers, LdapUsers: spaceConfig.SpaceDevLDAPUsers},
+		Manager:                 space.UserMgmt{LdapGroup: spaceConfig.SpaceMgrLDAPGrp, Users: spaceConfig.SpaceMgrUAAUsers, LdapUsers: spaceConfig.SpaceMgrLDAPUsers},
+		Auditor:                 space.UserMgmt{LdapGroup: spaceConfig.SpaceAuditorLDAPGrp, Users: spaceConfig.SpaceAuditorUAAUsers, LdapUsers: spaceConfig.SpaceAuditorLDAPUsers},
+		EnableSpaceQuota:        spaceConfig.SpaceQuota.IsQuotaEnabled(),
+		MemoryLimit:             spaceConfig.SpaceQuota.GetMemoryLimit(),
+		InstanceMemoryLimit:     spaceConfig.SpaceQuota.GetInstanceMemoryLimit(),
+		TotalRoutes:             spaceConfig.SpaceQuota.GetTotalRoutes(),
+		TotalServices:           spaceConfig.SpaceQuota.GetTotalServices(),
+		PaidServicePlansAllowed: spaceConfig.SpaceQuota.IsPaidServicesAllowed(),
+		RemoveUsers:             true,
+		AllowSSH:                spaceConfig.AllowSSH,
+	}
+	mgr.WriteFile(fmt.Sprintf("%s/%s/%s/spaceConfig.yml", m.ConfigDir, orgName, spaceName), spaceConfigYml)
+	mgr.WriteFileBytes(fmt.Sprintf("%s/%s/%s/security-group.json", m.ConfigDir, orgName, spaceName), []byte("[]"))
+	return nil
 }
 
 //CreateConfigIfNotExists Create org and space config directory. If directory already exists, it is left as is
 func (m *DefaultManager) CreateConfigIfNotExists(uaaOrigin string) error {
-	var err error
-	utilsManager := utils.NewDefaultManager()
-	if !utilsManager.FileOrDirectoryExists(m.ConfigDir) {
-		if err = os.MkdirAll(m.ConfigDir, 0755); err == nil {
-			lo.G.Infof("Config directory %s created", m.ConfigDir)
-			utilsManager.WriteFile(fmt.Sprintf("%s/ldap.yml", m.ConfigDir), &ldap.Config{TLS: false, Origin: uaaOrigin})
-			utilsManager.WriteFile(fmt.Sprintf("%s/orgs.yml", m.ConfigDir), &organization.InputOrgs{})
-			utilsManager.WriteFile(fmt.Sprintf("%s/spaceDefaults.yml", m.ConfigDir), &space.ConfigSpaceDefaults{})
-		} else {
-			lo.G.Errorf("Error creating config directory %s. Error : %s", m.ConfigDir, err)
-		}
-	} else {
+	mgr := utils.NewDefaultManager()
+	if mgr.FileOrDirectoryExists(m.ConfigDir) {
 		lo.G.Infof("Config directory %s already exists, skipping creation", m.ConfigDir)
+		return nil
 	}
-	return err
+	if err := os.MkdirAll(m.ConfigDir, 0755); err != nil {
+		lo.G.Errorf("Error creating config directory %s. Error : %s", m.ConfigDir, err)
+		return fmt.Errorf("cannot create directory %s: %v", m.ConfigDir, err)
+	}
+	lo.G.Infof("Config directory %s created", m.ConfigDir)
+	mgr.WriteFile(fmt.Sprintf("%s/ldap.yml", m.ConfigDir), &ldap.Config{TLS: false, Origin: uaaOrigin})
+	mgr.WriteFile(fmt.Sprintf("%s/orgs.yml", m.ConfigDir), &organization.InputOrgs{})
+	mgr.WriteFile(fmt.Sprintf("%s/spaceDefaults.yml", m.ConfigDir), &space.ConfigSpaceDefaults{})
+	return nil
 }
 
 //DeleteConfigIfExists Deletes config directory if it exists
 func (m *DefaultManager) DeleteConfigIfExists() error {
-	var err error
 	utilsManager := utils.NewDefaultManager()
-	if utilsManager.FileOrDirectoryExists(m.ConfigDir) {
-		err = os.RemoveAll(m.ConfigDir)
-		if err != nil {
-			lo.G.Errorf("Error deleting config folder. Error : %s", err)
-			return err
-		}
-		lo.G.Info("Config directory deleted")
-	} else {
+	if !utilsManager.FileOrDirectoryExists(m.ConfigDir) {
 		lo.G.Infof("%s doesn't exists, nothing to delete", m.ConfigDir)
+		return nil
 	}
-	return err
+	if err := os.RemoveAll(m.ConfigDir); err != nil {
+		lo.G.Errorf("Error deleting config folder. Error: %s", err)
+		return fmt.Errorf("cannot delete %s: %v", m.ConfigDir, err)
+	}
+	lo.G.Info("Config directory deleted")
+	return nil
 }
