ea259e1a9b295cb149c41309886e05c8882e1e28
Zac Bergquist <zbergquist99@gmail.com>
2017-05-20 10:40:20 -0600

    UAA cleanup

--- uaa/uaa.go
+++ uaa/uaa.go
@@ -12,8 +12,8 @@ import (
 
 //Manager -
 type Manager interface {
-	GetCFToken(password string) (token string, err error)
-	GetUAACToken(secret string) (token string, err error)
+	GetCFToken(password string) (string, error)
+	GetUAACToken(secret string) (string, error)
 }
 
 //Token -
@@ -28,7 +28,7 @@ type DefaultUAAManager struct {
 }
 
 //NewDefaultUAAManager -
-func NewDefaultUAAManager(sysDomain, userID string) (mgr Manager) {
+func NewDefaultUAAManager(sysDomain, userID string) Manager {
 	return &DefaultUAAManager{
 		Host:   fmt.Sprintf("https://uaa.%s", sysDomain),
 		UserID: userID,
@@ -37,31 +37,29 @@ func NewDefaultUAAManager(sysDomain, userID string) (mgr Manager) {
 
 //GetCFToken -
 func (m *DefaultUAAManager) GetCFToken(password string) (string, error) {
-	request := gorequest.New()
 	tokenURL := fmt.Sprintf("%s/oauth/token", m.Host)
+	post := gorequest.New().Post(tokenURL)
+	post.TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	post.BasicAuth.Username = "cf"
+	post.BasicAuth.Password = ""
+
 	params := url.Values{
 		"grant_type":    {"password"},
 		"response_type": {"token"},
 		"username":      {m.UserID},
 		"password":      {password},
 	}
-	sendString := params.Encode()
-
-	post := request.Post(tokenURL)
-	post.TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
-	post.BasicAuth.Username = "cf"
-	post.BasicAuth.Password = ""
-	post.Send(sendString)
+	post.Send(params.Encode())
 	res, body, errs := post.End()
 	if len(errs) > 0 {
 		return "", errs[0]
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return "", fmt.Errorf(body)
+		return "", fmt.Errorf("cannot get CF token, error %v: %s", res.StatusCode, body)
 	}
 
-	t := new(Token)
+	t := Token{}
 	if err := json.Unmarshal([]byte(body), &t); err != nil {
 		return "", err
 	}
@@ -70,33 +68,30 @@ func (m *DefaultUAAManager) GetCFToken(password string) (string, error) {
 }
 
 //GetUAACToken -
-func (m *DefaultUAAManager) GetUAACToken(secret string) (token string, err error) {
-	var res *http.Response
-	var body string
-	var errs []error
+func (m *DefaultUAAManager) GetUAACToken(secret string) (string, error) {
 	request := gorequest.New()
-	tokenURL := fmt.Sprintf("%s/oauth/token", m.Host)
+	request.TargetType = "form"
+	post := request.Post(fmt.Sprintf("%s/oauth/token", m.Host))
+	post.TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	post.BasicAuth.Username = m.UserID
+	post.BasicAuth.Password = secret
+
 	params := url.Values{
 		"grant_type":    {"client_credentials"},
 		"response_type": {"token"},
 	}
-	sendString := params.Encode()
+	post.Send(params.Encode())
 
-	request.TargetType = "form"
-	post := request.Post(tokenURL)
-	post.TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
-	post.BasicAuth.Username = m.UserID
-	post.BasicAuth.Password = secret
-	post.Send(sendString)
-	if res, body, errs = post.End(); len(errs) == 0 && res.StatusCode == http.StatusOK {
-		t := new(Token)
-		if err = json.Unmarshal([]byte(body), &t); err == nil {
-			token = t.AccessToken
-		}
-	} else if len(errs) > 0 {
-		err = errs[0]
-	} else {
-		err = fmt.Errorf(body)
+	res, body, errs := post.End()
+	if len(errs) > 0 {
+		return "", errs[0]
 	}
-	return
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("cannot get UAAC token, error %v: %s", res.StatusCode, body)
+	}
+	t := Token{}
+	if err := json.Unmarshal([]byte(body), &t); err != nil {
+		return "", fmt.Errorf("cannot read token: %v", err)
+	}
+	return t.AccessToken, nil
 }
