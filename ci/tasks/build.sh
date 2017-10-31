91449d0a94139b8a180b2169ae75bb81c9c4f25c
Caleb Washburn <cwashburn@pivotal.io>
2017-10-31 00:08:09 -0700

    fix issue with version number not being passed to binary

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -27,12 +27,12 @@ cp -R ${SOURCE_DIR}/* ${WORKING_DIR}/.
 cd ${WORKING_DIR}
 glide install
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-linux -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt/main.go
-GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-osx -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt/main.go
-GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt.exe -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt/main.go
+GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-osx -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt/main.go
+GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt.exe -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt/main.go
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-config-linux -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt-config/main.go
-GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-config-osx -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt-config/main.go
-GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-config.exe -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt-config/main.go
+GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-config-osx -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt-config/main.go
+GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/cf-mgmt-config.exe -ldflags "-X github.com/pivotalservices/cf-mgmt/commands.VERSION=${DRAFT_VERSION}" cmd/cf-mgmt-config/main.go
 
 echo ${DRAFT_VERSION} > ${OUTPUT_DIR}/name
 echo ${DRAFT_VERSION} > ${OUTPUT_DIR}/tag
