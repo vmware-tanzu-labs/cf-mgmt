3c88f8ea7751ecaf3efe2fde6a6619ab19e290a5
Caleb Washburn <cwashburn@pivotal.io>
2017-10-31 19:54:15 -0700

    removing duplicate -config from command names

--- commands/cf_mgmt_config.go
+++ commands/cf_mgmt_config.go
@@ -2,10 +2,10 @@ package commands
 
 type CfMgmtConfigCommand struct {
 	Version                          VersionCommand                   `command:"version" description:"Print version information and exit"`
-	InitConfigurationCommand         InitConfigurationCommand         `command:"init-config" description:"Initializes folder structure for configuration"`
-	AddOrgToConfigurationCommand     AddOrgToConfigurationCommand     `command:"add-org-to-config" description:"Adds specified org to configuration"`
-	AddSpaceToConfigurationCommand   AddSpaceToConfigurationCommand   `command:"add-space-to-config" description:"Adds specified space to configuration for org"`
-	ExportConfigurationCommand       ExportConfigurationCommand       `command:"export-config" description:"Exports org and space configurations from an existing Cloud Foundry instance. [Warning: This operation will delete existing config folder]"`
+	InitConfigurationCommand         InitConfigurationCommand         `command:"init" description:"Initializes folder structure for configuration"`
+	AddOrgToConfigurationCommand     AddOrgToConfigurationCommand     `command:"add-org" description:"Adds specified org to configuration"`
+	AddSpaceToConfigurationCommand   AddSpaceToConfigurationCommand   `command:"add-space" description:"Adds specified space to configuration for org"`
+	ExportConfigurationCommand       ExportConfigurationCommand       `command:"export" description:"Exports org and space configurations from an existing Cloud Foundry instance. [Warning: This operation will delete existing config folder]"`
 	GenerateConcoursePipelineCommand GenerateConcoursePipelineCommand `command:"generate-concourse-pipeline" description:"generates a concourse pipline to be used to drive cf-mgmt"`
 }
 
