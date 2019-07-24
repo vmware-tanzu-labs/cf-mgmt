07b926a3d0c39ca145e2edf0a8aa10c88c5dd7ed
Caleb Washburn <cwashburn@pivotal.io>
2019-07-23 21:22:25 -0700

    enabling generate concourse pipeline to take in target director and configuration directory are args - https://github.com/pivotalservices/cf-mgmt/issues/178

---
+++ configcommands/generate_concourse_pipeline_test.go
@@ -0,0 +1,39 @@
+package configcommands_test
+
+import (
+	"path"
+
+	. "github.com/pivotalservices/cf-mgmt/configcommands"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("GenerateConcoursePipeline", func() {
+	var (
+		command         *GenerateConcoursePipelineCommand
+		targetDirectory = "./_testGen"
+		configDirectory = "./_testGen/config"
+	)
+	BeforeEach(func() {
+		command = &GenerateConcoursePipelineCommand{}
+		command.TargetDirectory = targetDirectory
+		command.ConfigDirectory = configDirectory
+	})
+	AfterEach(func() {
+		// err := os.RemoveAll(configDirectory)
+		// Expect(err).ShouldNot(HaveOccurred())
+		// err = os.RemoveAll(targetDirectory)
+		// Expect(err).ShouldNot(HaveOccurred())
+	})
+	Context("Should generate a concourse pipeline and tasks", func() {
+		It("Should Succeed", func() {
+			err := command.Execute(nil)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(path.Join(targetDirectory, "ci", "tasks", "cf-mgmt.sh")).To(BeAnExistingFile())
+			Expect(path.Join(targetDirectory, "ci", "tasks", "cf-mgmt.yml")).To(BeAnExistingFile())
+			Expect(path.Join(targetDirectory, "pipeline.yml")).To(BeAnExistingFile())
+			Expect(path.Join(configDirectory, "vars.yml")).To(BeAnExistingFile())
+		})
+	})
+})
