91cdac241c9d521465a3716e4f730e17238c9b51
Caleb Washburn <cwashburn@pivotal.io>
2019-10-24 12:27:53 -0700

    ensure test coverage of merging protected orgs

--- config/org_test.go
+++ config/org_test.go
@@ -18,4 +18,22 @@ var _ = Describe("Org", func() {
 			Expect(len(org.Orgs)).To(Equal(2))
 		})
 	})
+
+	Context("Should merge protected org list with default protected orgs", func() {
+		It("should not include duplicates", func() {
+			org := &Orgs{
+				ProtectedOrgs: []string{"p-dataflow", "protect-me"},
+			}
+			protectedOrgList := org.ProtectedOrgList()
+			Expect(protectedOrgList).Should(HaveLen(8))
+			Expect(protectedOrgList).Should(ContainElement("system"))
+			Expect(protectedOrgList).Should(ContainElement("p-spring-cloud-services"))
+			Expect(protectedOrgList).Should(ContainElement("splunk-nozzle-org"))
+			Expect(protectedOrgList).Should(ContainElement("redis-test-ORG*"))
+			Expect(protectedOrgList).Should(ContainElement("appdynamics-org"))
+			Expect(protectedOrgList).Should(ContainElement("credhub-service-broker-org"))
+			Expect(protectedOrgList).Should(ContainElement("p-dataflow"))
+			Expect(protectedOrgList).Should(ContainElement("protect-me"))
+		})
+	})
 })
