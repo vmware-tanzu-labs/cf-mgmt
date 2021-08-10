dcfcd82dd9dd70e560036d548e596550ae5e7a70
Neil Hickey <nhickey@vmware.com>
2021-08-10 14:43:45 -0600

    Fix uaa domain in integration tests
    - remove unneeded params
    
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- ci/tasks/runIntegrationTests.sh
+++ ci/tasks/runIntegrationTests.sh
@@ -2,21 +2,25 @@
 
 set -eu -o pipefail
 
-get_password_from_credhub() {
+get_from_credhub() {
   local variable_name=$1
   credhub find -j -n "${variable_name}" | jq -r .credentials[].name | xargs credhub get -j -n | jq -r .value
 }
 
+eval "$(bbl print-env --metadata-file cf-deployment-env/metadata)"
+
 go get code.cloudfoundry.org/uaa-cli
 
-uaa-cli target "https://uaa.${SYSTEM_DOMAIN}"
-uaa-cli get-client-credentials-token "admin" -s $(get_password_from_credhub cf_admin_password)
+uaa-cli target "https://uaa.${SYSTEM_DOMAIN}" -k
+uaa-cli get-client-credentials-token "admin" -s $(get_from_credhub uaa_admin_client_secret)
 
-uaa-cli create-client cf-mgmt \
-  --client_secret cf-mgmt-secret \
-  --authorized_grant_types client_credentials,refresh_token \
-  --authorities cloud_controller.admin,scim.read,scim.write,routing.router_groups.read
+if ! uaa-cli get-client cf-mgmt; then
+  uaa-cli create-client cf-mgmt \
+    --client_secret cf-mgmt-secret \
+    --authorized_grant_types client_credentials,refresh_token \
+    --authorities cloud_controller.admin,scim.read,scim.write,routing.router_groups.read
+fi
 
 pushd source > /dev/null
-  RUN_INTEGRATION_TESTS=true go test ./integration/...
+  RUN_INTEGRATION_TESTS=true go test ./integration/... -ginkgo.progress
 popd
