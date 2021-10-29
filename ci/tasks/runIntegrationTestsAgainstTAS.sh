9bdf59db6246f8e3d85723d90f4616a74f943dda
Diego Lemos <dlemos@vmware.com>
2021-10-29 16:15:29 +0100

    Push tooling installation to container image

--- ci/tasks/runIntegrationTestsAgainstTAS.sh
+++ ci/tasks/runIntegrationTestsAgainstTAS.sh
@@ -5,13 +5,7 @@ set -eu -o pipefail
 [ -d env ]
 [ -d source ]
 
-apt-get update
-apt-get install wget gnupg -y
-
-wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | apt-key add -
-echo "deb http://apt.starkandwayne.com stable main" | tee /etc/apt/sources.list.d/starkandwayne.list
-apt-get update
-apt-get install om -y
+: "${SYSTEM_DOMAIN:="$(jq -r '.sys_domain' env/metadata)"}"
 
 ADMIN_CLIENT_SECRET="$( \
   om \
@@ -22,12 +16,6 @@ ADMIN_CLIENT_SECRET="$( \
       --credential-reference .uaa.admin_client_credentials \
       --credential-field password \
 )"
-
-go get code.cloudfoundry.org/uaa-cli
-
-SYSTEM_DOMAIN="$(jq -r '.sys_domain' env/metadata)"
-uaa-cli target "https://uaa.${SYSTEM_DOMAIN}" -k
-uaa-cli get-client-credentials-token "admin" -s "$ADMIN_CLIENT_SECRET"
 
 if ! uaa-cli get-client cf-mgmt; then
   uaa-cli create-client cf-mgmt \
@@ -45,10 +33,6 @@ CF_ADMIN_PASSWORD="$( \
       --credential-reference .uaa.admin_credentials \
       --credential-field password \
 )"
-
-curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github&version=v6" | tar -zx
-mv cf /usr/local/bin
-cf -v
 
 pushd source > /dev/null
   export ADMIN_CLIENT_SECRET CF_ADMIN_PASSWORD SYSTEM_DOMAIN
