da477d857ff5d0216286a918a9cdb665016129e4
Niels de Troije <Niels.de.Troije@rabobank.nl>
2023-01-19 14:33:26 +0100

    AAD Groups for update-org-users

--- commands/update_org_users.go
+++ commands/update_org_users.go
@@ -3,16 +3,22 @@ package commands
 type UpdateOrgUsersCommand struct {
 	BaseCFConfigCommand
 	BaseLDAPCommand
+	BaseAzureADCommand
 	BasePeekCommand
 }
 
-//Execute - updates orgs quotas
+// Execute - updates orgs quotas
 func (c *UpdateOrgUsersCommand) Execute([]string) error {
 	if cfMgmt, err := InitializePeekManagers(c.BaseCFConfigCommand, c.Peek); err == nil {
 		if err := cfMgmt.UserManager.InitializeLdap(c.LdapUser, c.LdapPassword, c.LdapServer); err != nil {
 			return err
 		}
 		defer cfMgmt.UserManager.DeinitializeLdap()
+
+		if err := cfMgmt.UserManager.InitializeAzureAD(c.AadTennantId, c.AadClientId, c.AadSecret, c.AADUserOrigin); err != nil {
+			return err
+		}
+
 		return cfMgmt.UserManager.UpdateOrgUsers()
 	}
 	return nil
