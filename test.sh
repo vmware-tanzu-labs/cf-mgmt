a91f61dcceda9613c5bf558c9703495a36664fef
Caleb Washburn <cwashburn@vmware.com>
2023-07-13 14:16:33 -0700

    update security group management to use cc v3 api

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-source ~/dev/cf-mgmt-performance/.envrc
-export CONFIG_DIR=~/dev/cf-mgmt-performance/config
-go run cmd/cf-mgmt/main.go export-config --excluded-org system
-go run cmd/cf-mgmt/main.go apply
