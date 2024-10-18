4083a63e71f88cd2f967609c6dfe9813decb845a
Hongchol Sinn <83615060+hsinn0@users.noreply.github.com>
2024-10-18 16:21:44 -0700

    Update `concourse-url` (#580)

--- scripts/login_to_fly_and_save_target.sh
+++ scripts/login_to_fly_and_save_target.sh
@@ -2,7 +2,7 @@
 
 fly login \
 	--target='tpe-cf-mgmt' \
-	--concourse-url='https://tpe-concourse-rock.eng.vmware.com/' \
+	--concourse-url='https://tpe-concourse-rock.acc.broadcom.net/' \
 	--team-name='identity-and-credentials'
 
 fly -t tpe-cf-mgmt sync
